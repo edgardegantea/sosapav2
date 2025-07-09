db96b184622a473e3cecbc4c08b1aa4ef48d2efb
Edgar degante Aguilar <edegantea@Edgars-MacBook-Pro.local>
2025-07-08 22:17:58 -0600

    Actualización

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,8 +1,7 @@
 <footer class="pt-4 my-md-5 pt-md-5 border-top">
     <div class="row">
-        <div class="col-12 col-md"><img class="mb-2" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="24"
-                                        height="19"> <small class="d-block mb-3 text-body-secondary">&copy;
-                Gobierno de Tlatlauquitepec 2024-2027</small></div>
+        <div class="col-12 col-md"><img class="mb-2" src="<?php echo base_url('assets/img/sosapatlat.png'); ?>" alt="" width="80"> <small class="d-block mb-3 text-body-secondary">&copy;
+                SOSAPATLAT 2024-2027</small></div>
         <div class="col-6 col-md"><h5>Tlatlauquitepec</h5>
             <ul class="list-unstyled text-small">
                 <li class="mb-1"><a class="link-secondary text-decoration-none" target="_blank" href="https://ha-tlatlauquitepec.gob.mx"></a></li>
